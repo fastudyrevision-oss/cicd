22c6c85d4f1f269858b26578e4dc763116b40d42
Faizan Ali <fastudyrevision@gmail.com>
2026-02-22 23:38:02 +0500

    Fixed the docker demon issue added docker to ec2-user group and enable the docker at systemctl level

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "CI/CD is working 🚀" });
+  res.json({ message: "CI/CD is working 🚀 By faizan Ali" });
 });
 
 app.listen(3000, () => {
