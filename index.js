a17d90f4d05ecc8ee1f876612a04a13f8adbb239
Faizan Ali <fastudyrevision@gmail.com>
2026-02-23 00:00:50 +0500

    fixed port use by nginx , port:80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "CI/CD is working 🚀 By faizan Ali" });
+  res.json({ message: "CI/CD is working 🚀" });
 });
 
 app.listen(3000, () => {
